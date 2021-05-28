e1bcc9075626b91c1dcddc49ac3aff47566f21ab
darthshittious <varunj26012001@gmail.com>
2021-05-28 21:15:28 +0200

    Remove debug print

--- gpg_import.sh
+++ gpg_import.sh
@@ -2,7 +2,6 @@
 email=${1//*@varunj.me/root}
 email=${email//./dot}
 email=${email//@/at}
-echo $email
 curl https://varunj.me/pgp/${email}.asc | gpg --import
 if [[ $? -ne 0 ]]; then
     echo -e "\nThat doesn't look like one of my emails"
