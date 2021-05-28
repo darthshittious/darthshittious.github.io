d46482bdde3306288b51de6a24fadd3b806f0bf4
darthshittious <varunj26012001@gmail.com>
2021-05-28 21:08:45 +0200

    Fix import script

--- gpg_import.sh
+++ gpg_import.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 email=${1//*@varunj.me/root}
-email=${1//./dot}
+email=${email//./dot}
 email=${email//@/at}
+echo $email
 curl https://varunj.me/pgp/${email}.asc | gpg --import
 if [[ $? -ne 0 ]]; then
     echo -e "\nThat doesn't look like one of my emails"
