3b8e04a5ec051abcf351ecce2f5d3e61fc494f12
darthshittious <varunj26012001@gmail.com>
2021-05-28 19:40:40 +0200

    add shell script to import PGP keys

--- gpg_import.sh
+++ gpg_import.sh
@@ -2,7 +2,6 @@
 email=${1//./dot}
 email=${email//root*/root}
 email=${email//@/at}
-echo $email
 curl https://varunj.me/pgp/${email}.asc | gpg --import
 if [[ $? -ne 0 ]]; then
     echo -e "\nThat doesn't look like one of my emails"
